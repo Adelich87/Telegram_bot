3fcc07246886eeddc0a9e8b704d9fbbea5b6a8a4
Adelich87 <124904248+Adelich87@users.noreply.github.com>
2023-04-27 13:36:23 +0300

    Add_bot

---
+++ cryptobot/config.py
@@ -0,0 +1,10 @@
+
+TOKEN = '6064019435:AAGWJrA4Mkh6SDrT-dRgQAu9Ksc9IPDifH0'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'лира': 'TRY',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
